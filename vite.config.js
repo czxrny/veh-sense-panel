b84e2805e388fa5b4c1ec6739166f50f51074222
czxrny <sczarnecki091@gmail.com>
2025-11-03 20:50:02 +0100

    fix: correctly loading the .env file into the dev vite.config

--- vite.config.js
+++ vite.config.js
@@ -1,17 +1,21 @@
-import { defineConfig } from "vite";
+import { defineConfig, loadEnv } from "vite";
 import react from "@vitejs/plugin-react";
 
 // https://vite.dev/config/
-export default defineConfig({
-  plugins: [react()],
-  server: {
-    proxy: {
-      "/api": {
-        target: `${import.meta.env.VITE_BASE_URL}`,
-        changeOrigin: true,
-        secure: false,
-        rewrite: (path) => path.replace(/^\/api/, ""),
-      },
-    },
-  },
+  export default defineConfig(({ mode }) => {
+      const env = loadEnv(mode, process.cwd(), "");
+
+      return {
+        plugins: [react()],
+        server: {
+          proxy: {
+            "/api": {
+              target: env.VITE_BASE_URL,
+              changeOrigin: true,
+              secure: false,
+              rewrite: (path) => path.replace(/^\/api/, ""),
+            },
+          },
+        },
+      };
 });
